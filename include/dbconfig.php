e0b61b59d9a45e5d8b168844a605efd87f7f07fd
osm313 <pubglite7478528@gmail.com>
2020-08-18 21:17:12 +0500

    local

--- include/dbconfig.php
+++ include/dbconfig.php
@@ -3,7 +3,7 @@
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 try {
   //Username, Password and Database
-  $con = new mysqli("localhost", "root", "", "hunnn");
+  $con = new mysqli("ec2-52-73-247-67.compute-1.amazonaws.com", "ypenzdrdkzzkth", "9d00af100e4b56085b4c76df9ec1a40b868dff4a282ecddd832489b1c316b38a", "deoudr69um6i8j");
   $con->set_charset("utf8mb4");
 } catch(Exception $e) {
   error_log($e->getMessage());
